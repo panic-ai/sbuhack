b10488d31c42e11b3509c6b344ed7eaa6a273b3a
AnirudhDesai777 <anirudhdesai777@gmail.com>
2024-02-10 21:49:25 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -1,42 +1,27 @@
-const fs = require('fs');
-// Load images from the "user images" folder
-const imageContainer = document.querySelector('.image-container');
-
-// Assuming you have a way to get the list of image file names
-const imageFilenames = [''];
-const folderPath = 'userA_clothes';
-
-fs.readdir(folderPath, (err, files) => {
-    if (err) {
-        console.error('Error reading folder:', err);
-        return;
-    }
-
-    console.log('Files in userA_clothes folder:');
-    files.forEach(file => {
-        console.log(file);
+document.addEventListener('DOMContentLoaded', function() {
+    var gallery = document.querySelector('.gallery');
+    var basePath = 'userA_clothes/';
+  
+    // Fetch the list of images from the server-side script
+    fetch('list_images.php') // Assuming 'list_images.php' outputs the list of image URLs
+      .then(response => response.json()) // Expecting the server to return a JSON array
+      .then(images => {
+        // Add images to gallery
+        images.forEach(function(imageSrc) {
+          var img = document.createElement('img');
+          img.src = basePath + imageSrc;
+          img.alt = 'User Image';
+          gallery.appendChild(img);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching images:', error);
+      });
+    
+    var addClozyButton = document.getElementById('addClozy');
+    addClozyButton.addEventListener('click', function() {
+      console.log('Add new item functionality here');
+      // Implement the functionality to add new items
     });
-});
-
-imageFilenames.forEach(filename => {
-    const img = document.createElement('img');
-    img.src = `user-images/${filename}`;
-    img.alt = '';
-    imageContainer.appendChild(img);
-});
-
-// Add event listeners to buttons
-const homeButton = document.querySelector('.home-button');
-homeButton.addEventListener('click', () => {
-    // Implement home button functionality
-});
-
-const settingsButton = document.querySelector('.settings-button');
-settingsButton.addEventListener('click', () => {
-    // Implement settings button functionality
-});
-
-const addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', () => {
-    // Implement add more functionality
-});
+  });
+  
