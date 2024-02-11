661fd78d6d252e4f68b286eee71d3552c5027af5
Sri harsha Patallapalli <patallapalli.s@northeastern.edu>
2024-02-11 01:44:00 -0500

    adj

--- src/main.py
+++ src/main.py
@@ -74,6 +74,19 @@ def complete_process(image):
     outputs = model(**inputs)
     return save_segmented_parts(image, outputs, threshold=0.5)
 
+def fix_channels(t):
+    """
+    Some images may have 4 channels (transparent images) or just 1 channel (black and white images), in order to let the images have only 3 channels. I am going to remove the fourth channel in transparent images and stack the single channel in back and white images.
+    :param t: Tensor-like image
+    :return: Tensor-like image with three channels
+    """
+    if len(t.shape) == 2:
+        return ToPILImage()(torch.stack([t for i in (0, 0, 0)]))
+    if t.shape[0] == 4:
+        return ToPILImage()(t[:3])
+    if t.shape[0] == 1:
+        return ToPILImage()(torch.stack([t[0] for i in (0, 0, 0)]))
+    return ToPILImage()(t)
 
 @app.get("/clozy/UI/{imageasset}")
 async def get_image_assets(imageasset: str):
